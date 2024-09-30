d1b0a6dffa81561add8317c09794db8c5ff10685
Shalvex Novachrono <55297590+ShalvexNovachrono@users.noreply.github.com>
2024-09-30 23:37:45 +0100

    first commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Random Number Game
+
+Enter your guess for a number, and the code will determine whether it matches the random number or not. 
+
+Your guess will be the winning number.
+
+However, you'll be told to guess higher if your guess is less than the random number.
+
+but you'll be told to guess lower if your guess is higher than the random number.
+
+![Quick Peak](image.png)
---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <typeinfo>
+#include <random>
+
+using namespace std;
+
+void print(string message) {
+    cout << message << endl;
+}
+
+void printNoNewLine(string message) {
+    cout << message;
+}
+
+string input(string message) {
+    printNoNewLine(message);
+    string thisInput;
+    cin >> thisInput;
+    return thisInput;
+}
+
+int GenRandomInt(int MIN, int MAX) {
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(1, 10);
+    return dis(gen);
+}
+
+
+
+bool IsNumberFound = false;
+int main() {
+    print("Guess a number from 1 - 10");
+    int RandomNumber = GenRandomInt(1, 10);
+
+    while (!IsNumberFound) {
+        string InputS = input(">>> ");
+        try {
+            int InputI = stoi(InputS);
+            if (InputI == RandomNumber) {
+                print("You guessed it!");
+                IsNumberFound = true;
+                break;
+            } else if (InputI > RandomNumber) {
+                print("Guess lower!");
+            } else {
+                print("Guess higher!");
+            }
+        } catch (exception e) {
+            print("Invalid input. Please enter a number. #Error");
+        }
+    }
+
+}
